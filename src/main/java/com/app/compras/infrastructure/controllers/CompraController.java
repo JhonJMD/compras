262778c6d99595e136b4b58f8bf1e8d7ab137e45
JhonJMD <jjmdcampus@gmail.com>
2025-05-12 18:04:57 -0500

    fix: :bug: Fix cors

--- src/main/java/com/app/compras/infrastructure/controllers/CompraController.java
+++ src/main/java/com/app/compras/infrastructure/controllers/CompraController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/compra")
+@CrossOrigin(origins = "http://localhost:5173")
 @Tag(name = "Compras", description = "API para la gestión de compras")
 public class CompraController {
     @Autowired
